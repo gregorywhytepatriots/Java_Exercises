38d8eca45a358b3a4de812ab1e53f191dca9a5c9
gregorywhytepatriots <49684423+gregorywhytepatriots@users.noreply.github.com>
2019-10-23 11:45:58 -0400

    MyApp

---
+++ My_App/src/model/com/Product.kt
@@ -0,0 +1,12 @@
+package model.com
+
+object Product {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello World")
+    }
+}
+
+
+
---
+++ My_App/src/model/com/test.kt
@@ -0,0 +1,3 @@
+package model.com
+
+class test
